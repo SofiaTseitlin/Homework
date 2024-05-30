0234623df3dc833962cefe126768ecc1e5df930e
SofiaTseitlin <sofia.zeitlin@gmail.com>
2024-05-30 17:58:13 +0300

    Homework 1. Task 2

---
+++ Homework1/Task3.py
@@ -0,0 +1,10 @@
+number = input()
+while not number.isdigit():
+    number = input("Введите число: ")
+summa = int(number)
+while summa > 9:
+    summa = 0
+    for i in number:
+        summa += int(i)
+    number = str(summa)
+print(summa)
