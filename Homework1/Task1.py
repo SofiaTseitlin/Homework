093b1ea8d6a9675a6cf0c21da13905dc45e3a5d9
SofiaTseitlin <sofia.zeitlin@gmail.com>
2024-05-30 17:06:02 +0300

    Homework 1. Task 1

---
+++ Homework1/Task1.py
@@ -0,0 +1,8 @@
+n = input("Введите желаемое количество строк для тругольника: ")
+while not n.isdigit():
+    n = input("Введите, пожалуйста, число: ")
+n = int(n)
+for i in range(n):
+    space = n - i - 1
+    stars = 2 * i + 1
+    print(" " * space + "*" * stars)
